f4214844d10adbe076abfbb1c8299683044d5bb8
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-03-27 23:36:19 +0530

    update fast

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,147 @@ import ArticleCard from '../components/ArticleCard'
 import BreakingNews from '../components/BreakingNews'
 
 export default function Home ({ articles }) {
+
+  const rankMathSchema = JSON.stringify({
+    '@context': 'https://schema.org',
+    '@graph': [
+      {
+        '@context': 'https://schema.org',
+        mainEntityOfPage: `${article.url}`,
+        '@type': 'NewsArticle',
+        url: `${article.url}`,
+        articleBody: `${article.articleBody}`,
+        articleSection: `${article.articleSection}`,
+        headline: `${article.title}`,
+        description: `${article.description}`,
+        keywords: [`${article.keywords}`],
+        author: {
+          '@type': 'Person',
+          name: `${article.author}`
+        },
+        datePublished: `${article.datePublished}`,
+        dateModified: `${article.dateModified}`,
+        image: {
+          '@type': 'ImageObject',
+          width: '1280px',
+          height: '720px',
+          url: `${article.image}`
+        },
+        publisher: {
+          '@type': 'Organization',
+          name: 'Crime Magazine',
+          logo: {
+            '@type': 'ImageObject',
+            width: '400px',
+            height: '100px',
+            url: 'https://crimemagazine.vercel.app/logo.png'
+          },
+          url: 'https://crimemagazine.vercel.app/'
+        }
+      },
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: [
+          {
+            '@type': 'ListItem',
+            position: '1',
+            item: {
+              '@id': 'https://crimemagazine.vercel.app',
+              name: 'Home'
+            }
+          },
+          {
+            '@type': 'ListItem',
+            position: '2',
+            item: {
+              '@id': 'https://crimemagazine.vercel.app/about',
+              name: 'About'
+            }
+          },
+          {
+            '@type': 'ListItem',
+            position: '3',
+            item: {
+              '@id': 'https://crimemagazine.vercel.app/contact',
+              name: 'Contact'
+            }
+          }
+        ]
+      }
+    ]
+  })
+
+  const blogSchema = JSON.stringify({
+    '@context': 'https://schema.org',
+    '@graph': [
+      {
+        '@type': ['Person', 'Organization'],
+        '@id': 'https://gravatar.com/drtrailer2022/#person',
+        name: 'Dr Trailer'
+      },
+      {
+        '@type': 'WebSite',
+        '@id': 'https://crimemagazine.vercel.app/#website',
+        url: 'https://crimemagazine.vercel.app/',
+        name: 'Crime Magazine™',
+        publisher: {
+          '@id': 'https://gravatar.com/drtrailer2022/#person'
+        },
+        inLanguage: 'en-US'
+      },
+      {
+        '@type': 'WebPage',
+        '@id': `${article.url}#webpage`,
+        url: `${article.url}`,
+        name: `${article.name} | Crime Magazine™`,
+        datePublished: `${article.datePublished}`,
+        dateModified: `${article.dateModified}`,
+        isPartOf: {
+          '@id': 'https://crimemagazine.vercel.app/#website'
+        },
+        inLanguage: 'en-US'
+      },
+      {
+        '@type': 'Person',
+        '@id': 'https://crimemagazine.vercel.app/author/crimemagazine/',
+        name: 'Dr Trailer',
+        url: 'https://crimemagazine.vercel.app/author/crimemagazine/',
+        image: {
+          '@type': 'ImageObject',
+          '@id': 'https://gravatar.com/drtrailer2022',
+          url: 'https://gravatar.com/drtrailer2022',
+          caption: 'Dr Trailer',
+          inLanguage: 'en-US'
+        },
+        sameAs: ['https://crimemagazine.vercel.app/']
+      },
+      {
+        '@type': 'BlogPosting',
+        '@id': `${article.url}#blogPost`,
+        headline: `${article.name} | Crime Magazine™`,
+        datePublished: `${article.datePublished}`,
+        dateModified: `${article.dateModified}`,
+        articleSection: `${article.articleSection}`,
+        author: {
+          '@id': 'https://crimemagazine.vercel.app/author/crimemagazine/'
+        },
+        publisher: {
+          '@id': 'https://gravatar.com/drtrailer2022/#person'
+        },
+        description:
+          'Welcome to Crime Magazine, your premier destination for in-depth coverage of crime-related news in India. Our platform curates the most compelling stories from reliable sources, offering a comprehensive look into the world of crime. Through a blend of audio, video, and images sourced from reputable channels, we strive to present the truth behind each incident. From high-profile cases to lesser-known events, we delve deep into the details, providing our readers with a nuanced understanding of the crime landscape in India. Our commitment to accuracy and reliability ensures that every story we deliver is thoroughly researched and fact-checked. Join us as we uncover the truth behind the headlines and explore the realities of the crime world in India.',
+        image: article.image,
+        name: `${article.name} | Crime Magazine™`,
+        '@id': `${article.url}#richSnippet`,
+        isPartOf: {
+          '@id': `${article.url}#webpage`
+        },
+        inLanguage: 'en-US'
+      }
+    ]
+  })
+
   return (
     <div
       className='w-full  mx-auto py-8 px-4'
@@ -44,6 +185,14 @@ export default function Home ({ articles }) {
         <meta name='robots' content='index, follow, noodp, noydir' />
         <link rel='canonical' href='https://crimemagazine.vercel.app/' />
         <meta name="google-site-verification" content="49WAkeHVyYLOO41vqw4JPOE4QOR244MUbadgZ4k3WcE" />
+        <script
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{ __html: blogSchema }}
+        />
+        <script
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{ __html: rankMathSchema }}
+        />
       </Head>
 
       <main className='container mx-auto py-8 px-4'>
