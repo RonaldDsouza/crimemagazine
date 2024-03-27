69ea2fb55b540d2a996d4c196e257eea76a2d189
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-03-28 00:14:19 +0530

    hit hard

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,108 @@ import ArticleCard from '../components/ArticleCard'
 import BreakingNews from '../components/BreakingNews'
 
 export default function Home ({ articles }) {
+
+  const rankMathSchema = `
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Crime Magazine | The Darkest Crime And Evil Minds.",
+  "url": "https://crimemagazine.vercel.app/",
+  "logo": "https://crimemagazine.vercel.app/logo.png",
+  "description": "Welcome to Crime Magazine. Our premier destination for in-depth coverage of crime-related news in India.",
+  "sameAs": [
+    "https://crimemagazine.vercel.app/",
+    "https://gravatar.com/drtrailer2022"
+  ],
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://crimemagazine.vercel.app/search/{search_term_string}",
+    "query-input": "required name=search_term_string"
+  },
+  "author": {
+    "@type": "Person",
+    "name": "Dr Trailer",
+    "url": "https://crimemagazine.vercel.app/author/crimemagazine/",
+    "image": {
+      "@type": "ImageObject",
+      "url": "https://gravatar.com/drtrailer2022"
+    }
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "Crime Magazine",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://crimemagazine.vercel.app/logo.png"
+    }
+  }
+}
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "url": "https://crimemagazine.vercel.app/",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://crimemagazine.vercel.app/search/{search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
+}
+{
+  "@context": "https://schema.org",
+  "@type": "ItemList",
+  "itemListElement": [
+    {
+      "@type": "SiteNavigationElement",
+      "position": 1,
+      "name": "Home",
+      "description": "Welcome to Crime Magazine",
+      "url": "https://crimemagazine.vercel.app/"
+    },
+    {
+      "@type": "SiteNavigationElement",
+      "position": 2,
+      "name": "About",
+      "description": "Our premier destination for in-depth coverage of crime-related news in India.",
+      "url": "https://crimemagazine.vercel.app/about/"
+    },
+    {
+      "@type": "SiteNavigationElement",
+      "position": 3,
+      "name": "Contact",
+      "description": "Contact",
+      "url": "https://crimemagazine.vercel.app/contact/"
+    }
+  ]
+}
+{
+  "@context": "https://schema.org",
+  "@type": "NewsArticle",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://crimemagazine.vercel.app/article/my-article"
+  },
+  "headline": "Crime Magazine | The Darkest Crime And Evil Minds.",
+  "image": [
+    "https://crimemagazine.vercel.app/article-image.jpg"
+   ],
+  "datePublished": "2024-03-27T08:00:00Z",
+  "dateModified": "2024-03-28T09:20:00Z",
+  "author": {
+    "@type": "Person",
+    "name": "Dr Trailer"
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "Crime Magazine",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://crimemagazine.vercel.app/logo.png"
+    }
+  },
+  "description": " Welcome to Crime Magazine, your premier destination for in-depth coverage of crime-related news in India. Our platform curates the most compelling stories from reliable sources, offering a comprehensive look into the world of crime. Through a blend of audio, video, and images sourced from reputable channels, we strive to present the truth behind each incident."
+}
+`;
+
   return (
     <div
       className='w-full  mx-auto py-8 px-4'
@@ -21,7 +123,7 @@ export default function Home ({ articles }) {
           name='description'
           content='Crime Magazine | The Darkest Crime And Evil Minds.'
         />
-        <meta name='keywords' content={articles && articles.keywords} />
+        <meta name='keywords' content="india news, india news headlines, india local news, india daily news, india newspaper, latest news, india headlines, india news today, india news, top news, breaking news, current news, current news updates, news updates, online newspaper, bollywood, sports, life and style, crime, news portal "/>
 
         <meta
           property='og:title'
@@ -44,6 +146,11 @@ export default function Home ({ articles }) {
         <meta name='robots' content='index, follow, noodp, noydir' />
         <link rel='canonical' href='https://crimemagazine.vercel.app/' />
         <meta name="google-site-verification" content="49WAkeHVyYLOO41vqw4JPOE4QOR244MUbadgZ4k3WcE" />
+     
+        <script
+          type='application/ld+json'
+          dangerouslySetInnerHTML={{ __html: rankMathSchema }}
+        />
       </Head>
 
       <main className='container mx-auto py-8 px-4'>
